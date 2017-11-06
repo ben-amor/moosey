3b114dc42f669f3f7e80c84bd683d3d5a7eb9b65
Ben Amor <ben@showgizmo.com>
2017-11-06 23:02:02 +1300

    Added relevant nuget package

--- Moosey/MooseyPhotoProvider.cs
+++ Moosey/MooseyPhotoProvider.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using Google.Apis.Drive.v3;
 
 namespace Moosey
 {
@@ -9,6 +10,8 @@ namespace Moosey
     {
         public IEnumerable<object> GetMostRecentMooseyPhotoSince(DateTime dateTime)
         {
+            // TODO large preamble from https://developers.google.com/drive/v3/web/quickstart/dotnet
+            var driveService = new DriveService();
 
         }
     }
