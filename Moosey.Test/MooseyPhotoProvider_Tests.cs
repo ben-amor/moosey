51b29c888b629d09ea89296beb6b5cfb5966428a
Ben Amor <ben@showgizmo.com>
2017-11-06 22:54:43 +1300

    Initial commit; no functionality

---
+++ Moosey.Test/MooseyPhotoProvider_Tests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Moosey.Test
+{
+    public class MooseyPhotoProvider_Tests
+    {
+        [Test]
+        public void A_mooey_photo_can_be_retrieved()
+        {
+            // Arrange
+            var sut = new MooseyPhotoProvider();
+
+            // Act
+            var result = sut.GetMostRecentMooseyPhotoSince(DateTime.Now);
+
+            // Assert
+            Assert.That(result, Is.Not.Null);
+        }
+    }
+}
---
+++ Moosey/MooseyPhotoProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Moosey
+{
+    public class MooseyPhotoProvider
+    {
+        public IEnumerable<object> GetMostRecentMooseyPhotoSince(DateTime dateTime)
+        {
+
+        }
+    }
+    }
+}
